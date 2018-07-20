4a998620bcfc41c67889e2be4c63f38773ba295d
Lucho <luo.cortes@gmail.com>
2018-07-20 17:19:00 -0500

    Se inserta NodeJs

---
+++ NodeJs/Practicas/holamundo.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+var manejador = function(solicitud, respuesta){
+	console.log("Hola mundo");
+	respuesta.end();
+};
+
+var servidor = http.createServer(manejador);
+
+servidor.listen(8080);
